7035e7a0a3a40f5284c5169af18daa3d68f044bb
Matthew Ronald Reed <u19100133@tuks.co.za>
2022-11-17 09:25:20 +0200

    🚧 (T1): Added new properties to Accounts

--- Accounts.cs
+++ Accounts.cs
@@ -1,11 +1,21 @@
-/* namespace matthew_prac;
+/* using System;
+
+namespace matthew_prac;
 
 public class Accounts
 {
+    private Guid uid;
     private string uname;
     private string pword;
 
+    public Accounts(string u, string p)
+    {
+        uid = new Guid();
+        Username = u;
+        Password = p;
+    }
+
     public string Username {get{return uname;}; set{uname = value;};};
-    public string password {get{return pword;}; set{pword = value;};};
+    public string Password {get{return pword;}; set{pword = value;};};
 }
  */
